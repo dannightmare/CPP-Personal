287bd9371b3f6458e658c19748242d458f00a780
dannightmare <dan.kusai.star.123234@gmail.com>
2023-01-02 15:31:59 +0200

    made QuickSort

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,3 +1,33 @@
+#include "QuickSort.h"
+
 void
 QuickSort(int* arr, int p, int r)
-{}
+{
+    if (p >= r)
+        return;
+    int pivot = arr[p];
+    int i = p - 1;
+    int j = r + 1;
+    while (i <= j) {
+        do {
+            i++;
+        } while (i < r && arr[i] < pivot);
+        do {
+            j--;
+        } while (j > p && arr[j] > pivot);
+        if (i < j)
+            swap(arr[i], arr[j]);
+        else
+            break;
+    }
+    QuickSort(arr, p, j);
+    QuickSort(arr, j + 1, r);
+}
+
+void
+swap(int& x, int& y)
+{
+    int tmp = x;
+    x = y;
+    y = tmp;
+}
--- QuickSort/QuickSort.h
+++ QuickSort/QuickSort.h
@@ -1,3 +1,5 @@
 void QuickSort(int* arr, int p, int r);
 
 void UnsortedIndex(int* arr, int p, int r, int i);
+
+void swap(int& x, int& y);
